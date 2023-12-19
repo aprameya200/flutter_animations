cb5266db8284a7d0fba8b98d45deaba661958bc4
aprameyaneopane <aprameyaneopane@gmail.com>
2023-12-19 17:52:42 +0545

    sign in box

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -36,6 +36,7 @@ class _StartPageState extends State<StartPage> {
         statusBarColor: Colors.transparent));
 
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Stack(
         children: [
           Positioned(
@@ -85,13 +86,221 @@ class _StartPageState extends State<StartPage> {
                       child: GestureDetector(
                         onTap: () {
                           _btnAnimationController.isActive = true;
-                          showGeneralDialog( //shows the dialog box
+                          showGeneralDialog(
+                              barrierDismissible: true,
+                              //dismissable
+                              barrierLabel: MaterialLocalizations.of(context)
+                                  .modalBarrierDismissLabel,
+                              barrierColor: Colors.black.withOpacity(0.5),
+                              // Color of the barrier
+                              transitionDuration: Duration(milliseconds: 300),
+                              //shows the dialog box
                               context: context,
-                              pageBuilder: (context, _, __) => Container(
-                                height: 100,
-                                    width: 300,
-                                    color: Colors.red,
-                                    child: Text("AA"),
+                              pageBuilder: (context, _, __) => Center(
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                          color: Colors.white,
+                                          borderRadius: BorderRadius.all(
+                                              Radius.circular(40))),
+                                      margin: EdgeInsets.symmetric(
+                                          horizontal: screenWidth * 0.1,
+                                          vertical: screenHeight * 0.17),
+                                      child: Scaffold(
+                                        resizeToAvoidBottomInset: true,
+                                        backgroundColor: Colors.transparent,
+                                        //allows to see back
+                                        body: Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                              horizontal: 26, vertical: 18.0),
+                                          child: Column(
+                                            children: [
+                                              Text(
+                                                "Sign In",
+                                                style: TextStyle(
+                                                    color: Colors.black,
+                                                    fontFamily: "Poppins",
+                                                    fontSize:
+                                                        screenHeight * 0.04,
+                                                    decoration:
+                                                        TextDecoration.none),
+                                              ),
+                                              SizedBox(
+                                                height: 10,
+                                              ),
+                                              Text(
+                                                "Efficiently retrieve desired information with streamlined access to content through intuitive interface.",
+                                                style: TextStyle(
+                                                    color: Colors.black,
+                                                    fontFamily: "Poppins",
+                                                    fontWeight: FontWeight.w100,
+                                                    fontSize:
+                                                        screenHeight * 0.014,
+                                                    decoration:
+                                                        TextDecoration.none),
+                                                textAlign: TextAlign.center,
+                                              ),
+                                              SizedBox(
+                                                height: 20,
+                                              ),
+                                              Align(
+                                                  alignment:
+                                                      AlignmentDirectional
+                                                          .centerStart,
+                                                  child: Text(
+                                                    "Email",
+                                                    style: TextStyle(
+                                                        fontSize: screenHeight *
+                                                            0.018,
+                                                        fontFamily: "Poppins"),
+                                                  )),
+                                              SizedBox(
+                                                height: 5,
+                                              ),
+                                              Container(
+                                                  width: screenWidth * 1.6,
+                                                  decoration:
+                                                      const BoxDecoration(
+                                                          color:
+                                                              Color(0xFFF5F5F5),
+                                                          borderRadius:
+                                                              BorderRadius.all(
+                                                                  Radius
+                                                                      .circular(
+                                                                          20))),
+                                                  child: Padding(
+                                                    padding: const EdgeInsets
+                                                        .symmetric(
+                                                        horizontal: 24.0),
+                                                    child: Row(
+                                                      mainAxisAlignment:
+                                                          MainAxisAlignment
+                                                              .start,
+                                                      children: [
+                                                        Icon(
+                                                          CupertinoIcons.mail,
+                                                          size: 30,
+                                                        ),
+                                                        SizedBox(
+                                                          width: 10,
+                                                        ),
+                                                        SizedBox(
+                                                            width: screenWidth *
+                                                                0.43,
+                                                            child:
+                                                                TextFormField(
+                                                              style: TextStyle(
+                                                                  fontFamily:
+                                                                      "Poppins"),
+                                                              decoration: const InputDecoration(
+                                                                  border:
+                                                                      InputBorder
+                                                                          .none,
+                                                                  hintText:
+                                                                      "Email"),
+                                                            )),
+                                                      ],
+                                                    ),
+                                                  )),
+                                              SizedBox(
+                                                height: 20,
+                                              ),
+                                              Align(
+                                                  alignment:
+                                                      AlignmentDirectional
+                                                          .centerStart,
+                                                  child: Text(
+                                                    "Password",
+                                                    style: TextStyle(
+                                                        fontSize: screenHeight *
+                                                            0.018,
+                                                        fontFamily: "Poppins"),
+                                                  )),
+                                              SizedBox(
+                                                height: 5,
+                                              ),
+                                              Container(
+                                                  width: screenWidth * 1.6,
+                                                  decoration:
+                                                      const BoxDecoration(
+                                                          color:
+                                                              Color(0xFFF5F5F5),
+                                                          borderRadius:
+                                                              BorderRadius.all(
+                                                                  Radius
+                                                                      .circular(
+                                                                          20))),
+                                                  child: Padding(
+                                                    padding: const EdgeInsets
+                                                        .symmetric(
+                                                        horizontal: 24.0),
+                                                    child: Row(
+                                                      mainAxisAlignment:
+                                                          MainAxisAlignment
+                                                              .start,
+                                                      children: [
+                                                        Icon(
+                                                          CupertinoIcons.lock,
+                                                          size: 30,
+                                                        ),
+                                                        SizedBox(
+                                                          width: 10,
+                                                        ),
+                                                        SizedBox(
+                                                            width: screenWidth *
+                                                                0.43,
+                                                            child:
+                                                                TextFormField(
+                                                              style: TextStyle(
+                                                                  fontFamily:
+                                                                      "Poppins"),
+                                                              decoration: const InputDecoration(
+                                                                  border:
+                                                                      InputBorder
+                                                                          .none,
+                                                                  hintText:
+                                                                      "Password"),
+                                                            )),
+                                                      ],
+                                                    ),
+                                                  )),
+                                              SizedBox(
+                                                width: screenWidth * 0.4,
+                                                height: screenHeight * 0.06,
+                                                child: ElevatedButton(
+                                                    style: ButtonStyle(
+                                                        backgroundColor:
+                                                            MaterialStatePropertyAll(
+                                                          Color(0xffFFB07F),
+                                                        ),
+                                                        elevation:
+                                                            MaterialStatePropertyAll(
+                                                                0)),
+                                                    onPressed: () {},
+                                                    child: Row(
+                                                      mainAxisAlignment:
+                                                          MainAxisAlignment
+                                                              .center,
+                                                      children: [
+                                                        Text("Sign In",
+                                                            style: TextStyle(
+                                                                fontFamily:
+                                                                    "Poppins",
+                                                                fontSize: screenHeight * 0.019,
+                                                                color: Colors
+                                                                    .black)),
+                                                        SizedBox(
+                                                          width: 10,
+                                                        ),
+                                                        Icon(CupertinoIcons
+                                                            .arrow_right,color: Colors.black,size: screenHeight * 0.037,)
+                                                      ],
+                                                    )),
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ),
                                   ));
                         },
                         child: Stack(children: [
